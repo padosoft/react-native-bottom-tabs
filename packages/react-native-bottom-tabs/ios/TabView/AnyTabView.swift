5e8b5caf6c541b296d6d1e4a53ca703ce85838bb
PADO <62028267+47PADO47@users.noreply.github.com>
2025-06-20 14:56:36 +0200

    fix: remove props from AnyTabView as AnyTabView is internal

--- packages/react-native-bottom-tabs/ios/TabView/AnyTabView.swift
+++ packages/react-native-bottom-tabs/ios/TabView/AnyTabView.swift
@@ -1,7 +1,6 @@
 import SwiftUI
 
 public protocol AnyTabView: View {
-  var props: TabViewProps { get }
   var onLayout: (_ size: CGSize) -> Void { get }
   var onSelect: (_ key: String) -> Void { get }
   var updateTabBarAppearance: () -> Void { get }
