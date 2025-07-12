14295bab199122f72f50d1741b507dcbdbb92922
PADO <62028267+47PADO47@users.noreply.github.com>
2025-07-12 18:33:25 +0200

    fix(android): assign role in RCTTabViewImpl

--- packages/react-native-bottom-tabs/android/src/main/java/com/rcttabview/RCTTabViewImpl.kt
+++ packages/react-native-bottom-tabs/android/src/main/java/com/rcttabview/RCTTabViewImpl.kt
@@ -36,7 +36,8 @@ class RCTTabViewImpl {
               badge = if (item.hasKey("badge")) item.getString("badge") else null,
               activeTintColor = if (item.hasKey("activeTintColor")) item.getInt("activeTintColor") else null,
               hidden = if (item.hasKey("hidden")) item.getBoolean("hidden") else false,
-              testID = item.getString("testID")
+              testID = item.getString("testID"),
+              role = item.getString("role"),
             )
           )
       }
